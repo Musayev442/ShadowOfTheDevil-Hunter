f6f2ed9f766050d87a2079ba37fdec2601c82e84
Murad <72296224+Musayev442@users.noreply.github.com>
2025-08-17 02:51:51 +0100

    Create Player.cs
    Player class added

---
+++ Assets/App/Code/Character/Player/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace SotD.Characters.Player
+{
+    /// <summary>Concrete player character. Extend with player-only data/behaviours.</summary>
+    public class Player : Character
+    {
+        /*[Header("Player")]
+        [SerializeField] private PlayerStats playerStats;
+        [SerializeField] private PlayerInventory inventory;
+        [SerializeField] private PlayerProgression progression;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            // Fetch/prepare any player-only modules here if needed later
+        }
+
+        public override void Initialize() { }
+        public override void TakeDamage(DamageInfo dmg) { }
+        public override void Die() { }
+        */
+    }
+}
